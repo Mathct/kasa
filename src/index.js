22bc0ddc59fcdff08e61f8fa10ac34667842e480
mathct <mathieu.chatrain@gmail.com>
2024-08-19 15:29:40 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import './styles/index.css'
 import Home from './pages/Home'
+import Test from './pages/Test'
+import Header from './components/Header'
+import Error from './components/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
