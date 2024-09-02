df43c6324b1118659c271b7009ff0f1ff58756ec
mathct <mathieu.chatrain@gmail.com>
2024-09-02 02:55:29 +0200

    rating

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -4,18 +4,18 @@ import starPink from '../../assets/star_pink.png';
 
 
 function Rating({ rating }) {
-    const stars = [];
+    const liststars = [];
 
     for (let i = 1; i <= 5; i++) {
         if (i <= rating) {
-            stars.push(<img key={i} src={starPink} alt="etoile" />);
+            liststars.push(<img key={i} src={starPink} alt="etoile" />);
         } else {
-            stars.push(<img key={i} src={starGrey} alt="vide" />);
+            liststars.push(<img key={i} src={starGrey} alt="vide" />);
         }
     }
 
     return (
-        <div className='rating'>{stars}</div>
+        <div className='etoiles'>{liststars}</div>
     );
 }
   
